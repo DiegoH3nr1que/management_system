b6fde48a2f5aeaa8f1071890e619bb342c505ba5
Vincenzo140 <vincenzo.amendola141@gmail.com>
2024-05-13 02:50:00 -0300

    chore: configure swagger endpoints

--- management_system/src/main/java/com/system/management_system/controller/UserController.java
+++ management_system/src/main/java/com/system/management_system/controller/UserController.java
@@ -4,6 +4,7 @@ import com.system.management_system.users.User;
 import com.system.management_system.users.UserRepository;
 import com.system.management_system.users.UserRequestDTO;
 import com.system.management_system.users.UserResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +20,8 @@ public class UserController {
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @PostMapping
+    @Operation(summary = "Postar", description = "Método que Posta os usuários", tags = "Post em todos os usuários")
+
     public void SaveUser(@RequestBody UserRequestDTO data){
         User UserData = new User(data);
         repository.save(UserData);
@@ -26,9 +29,11 @@ public class UserController {
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
+    @Operation(summary = "Listar", description = "Método que retorna os dados de usuário", tags = "Get em todos os usuários")
     public List<UserResponseDTO> getAll(){
         List<UserResponseDTO> userList = repository.findAll().stream().map(UserResponseDTO::new).toList();
         return userList;
+
     }
 
 }
